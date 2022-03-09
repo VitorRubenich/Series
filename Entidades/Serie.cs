610bf8843e62ed981ae8e8ae4c4b8d7d3eda5f91
VitorRubenich <rubenich.vitor@gmail.com>
2022-03-09 16:52:51 -0300

    Adicionar arquivos de projeto.

---
+++ Entidades/Serie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Series
+{
+    public class Serie : ClassBase
+    {
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+
+        private bool Excluido { get; set; }
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero" + this.Genero + Environment.NewLine;
+            retorno += "Titulo:"+ this.Titulo + Environment.NewLine;
+            retorno += "Descrição"+ this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: "+this.Ano + Environment.NewLine;
+            return retorno;
+        }
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+    }
+
+
+
+
+}
